1381b6f1af2fe73d3530ec4fac70fededdd71406
17586317 <Borisov.S.Alek@sberbank.ru>
2020-08-06 23:42:24 +0300

    Initial Commit

---
+++ NewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewApp
+//
+//  Created by 17586317 on 06.08.2020.
+//  Copyright © 2020 Борисов Сергей Александрович. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
