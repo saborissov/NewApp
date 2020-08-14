17149e9b90a99786d516aa02e1c6082d99075602
17586317 <Borisov.S.Alek@sberbank.ru>
2020-08-14 16:56:49 +0300

    addSourceCode

--- NewApp/ViewController.swift
+++ NewApp/ViewController.swift
@@ -1,20 +1,31 @@
-//
-//  ViewController.swift
-//  NewApp
-//
-//  Created by 17586317 on 06.08.2020.
-//  Copyright © 2020 Борисов Сергей Александрович. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var image: UIImageView!
+    
+    var container  = [1,2,3]
+    
+    @IBAction func getImage(_ sender: UIButton) {
+//       var firstArg = container[0]
+    
+        //https://picsum.photos/200
+//        URLSession.shared.dataTask(with: URL(string: "https://picsum.photos/200")!) { (data, response, error) in
+//           var data =  String(bytes: data!, encoding: .utf8)
+//        }
+        
+        URLSession.shared.downloadTask(with: URL(string: "https://picsum.photos/200")!) { (url, response, error) in
+            let data = try! Data(contentsOf: url!)
+            let avatar = UIImage(data: data)
+            DispatchQueue.main.async {
+                self.image.image = avatar
+            }
+            
+        }.resume()
+        print("get image")
+        
     }
-
-
+    
 }
 
